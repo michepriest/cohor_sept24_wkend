5c5788a13bf783c8410498d2d9c71177392c4eef
michepriest <miche.priest@gmail.com>
2018-09-30 21:03:27 +0000

    made a simple http request

---
+++ https_example.js
@@ -0,0 +1,17 @@
+const https = require("https");
+console.log("I did it!");
+
+const options = {
+  host: "www.example.org",
+  path: "/"
+};
+
+var callback = function() {
+  console.log("In response handler callback!");
+}
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
