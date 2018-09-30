3acb9f7fdac8140fcb79dea242ccba1b00e88189
michepriest <miche.priest@gmail.com>
2018-09-30 21:14:33 +0000

    modified response handler to read the data

--- https_example.js
+++ https_example.js
@@ -8,7 +8,11 @@ const options = {
 
 var callback = function(response) {
   console.log("In response handler callback!");
-  console.log("Response: ", response);
+
+  response.on("data", function(chunk) {
+    console.log("[-- CHUNK OF LENGTH " + chunk.length + " --]");
+    console.log(chunk.toString());
+  });
 }
 
 console.log("I'm about to make the request!");
